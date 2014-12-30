affb32c50121b18aa1a0f7254395e748f6b6089f
Kenneth Daily <kenneth.daily@sagebase.org>
2014-12-30 14:17:18 -0800

    Added roxygen documentation

--- R/rSynapseHelpers.R
+++ R/rSynapseHelpers.R
@@ -1,12 +1,17 @@
-# A better query, cleans the column names
+#' A better query, cleans the column names
+#' @export
+#' @param q A Synapse query
 betterQuery <- function(q, ...) {
-  qres <- synQuery(q, blockSize=100)
+  qres <- synapseClient::synQuery(q, blockSize=100)
   res <- qres$collectAll()
   colnames(res) <- gsub(".*\\.(.*)", "\\1", colnames(res))
   res
 }
 
-# Find records with missing data - blank, empty string, or NA
+#' Find records with missing data - blank, empty string, or NA
+#' @export
+#' @param res A data frame
+#' @param cols2check vector of column names
 findBlankRecords <- function(res, cols2check, ...) {
   badrecs <- apply(res[, cols2check], 1, function(x) any(is.na(x)) | any(x == "") | any(x == " "))
   res[badrecs, ]  
